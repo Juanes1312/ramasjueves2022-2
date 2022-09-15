6ebfb6903b10b5f92155a1b24c29efc469e5eefb
308 <308@academicos.cesde.edu.co>
2022-09-15 07:54:16 -0500

    proffe

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ print("Juan Jose Gallego---Vice JR")
 print("Trabjo Dago Molian---Desarrollador JR")
 
 print("Trabajo DSZ--desarrollador JR")
-print("Trabajo Juanes--desarrollador JR")
+print("Trabajo Juan Esteban Rincon el mejor--desarrollador JR")
 
